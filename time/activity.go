b91f7eb1e38e01f67f6ffc90b7eec917fe9f181f
lhollyer <32904354+lhollyer@users.noreply.github.com>
2017-12-11 14:47:45 +0000

    Add files via upload

---
+++ time/activity.go
@@ -0,0 +1,31 @@
+package time
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"time"
+)
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+
+	// do eval
+	t := time.Now()
+	fmt.Println(t.Format("20060102150405"))
+	context.SetOutput("result", t.Format("20060102150405"))
+	return true, nil
+}

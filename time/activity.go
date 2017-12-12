afd0d001c42744edd1472ff79fd29d5d29a18872
lhollyer <32904354+lhollyer@users.noreply.github.com>
2017-12-12 17:57:35 +0000

    Create activity.go

---
+++ time/activity.go
@@ -0,0 +1,34 @@
+package time
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"time"
+	"fmt"
+	"strconv"
+)
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+
+	// do eval
+	
+	t := time.Now()
+	fmt.Println(t.Hour())
+	context.SetOutput("result", strconv.Itoa(t.Hour()))
+	return true, nil
+}
